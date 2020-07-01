82075e70eaea4b825aa1140be56a93a01edb269b
Spejik <Jiriksperlik@seznam.cz>
2020-07-01 18:20:30 +0200

    nevím hele

--- bxc_os/http.cpp
+++ bxc_os/http.cpp
@@ -10,52 +10,19 @@ string Http::Get(string endpoint)
 {
 	curl_global_init(CURL_GLOBAL_ALL);
 
-	CURL* easyhandle = curl_easy_init();
-	std::string readBuffer;
+	CURL* curl = curl_easy_init();
+	string readBuffer;
 
-	curl_easy_setopt(easyhandle, CURLOPT_URL, _url + endpoint);
-	curl_easy_setopt(easyhandle, CURLOPT_VERBOSE, 1L);
-	curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, WriteCallback);
-	curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBuffer);
+	curl_easy_setopt(curl, CURLOPT_URL, _url + endpoint);
+	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &WriteCallback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 
-	curl_easy_perform(easyhandle);
+	curl_easy_perform(curl);
 
 	cout << readBuffer << endl;
 
 	return readBuffer;
-
-	/*auto curl = curl_easy_init();
-	if (curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, _url + endpoint);
-		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
-		curl_easy_setopt(curl, CURLOPT_USERPWD, _auth);
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
-		curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
-		curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
-
-		string response_string;
-		string header_string;
-
-		char* url;
-		long response_code;
-		double elapsed;
-		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-		curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
-		curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
-
-		curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-		curl = NULL;
-
-		cout << "Curl " << endpoint << ": " << response_string << endl;
-		return "";
-	}
-	else
-	{
-		cout << "curl_easy_init() failed" << endl;
-		throw new exception();
-		return "";
-	}*/
 }
 
 
--- bxc_os/http.h
+++ bxc_os/http.h
@@ -14,7 +14,7 @@ public:
 
 private:
 	string Get(string endpoint);
-	size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp);
+	static size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp);
 	string _url = "http://178.63.148.0:44214/"; 
 	string _auth = "app:gQKX5cWdRsYaWrqq";
 	string _REMOTE_VERSION = "";
